5e371d74644bc3245e96f914ac40e810dca30faa
Rabnawaz <rabnawaz12@gmail.com>
2025-05-09 02:50:17 -0700

    first commit

--- src/config/config.js
+++ src/config/config.js
@@ -1,11 +1,5 @@
-const config = {
-    development: {
-        API_URL: 'http://localhost:5000/api'
-    },
-    production: {
-        API_URL: 'https://videoappbackend-d4d9bhckaxg9eyhn.canadacentral-01.azurewebsites.net/api'
-    }
-};
+const API_URL = 'https://videoappbackend-d4d9bhckaxg9eyhn.canadacentral-01.azurewebsites.net/api';
 
-const environment = process.env.NODE_ENV || 'development';
-export default config[environment];
+export default {
+    API_URL
+}; 
